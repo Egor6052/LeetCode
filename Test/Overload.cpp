c00c32f01b4ee193bcd572781ced9487b80c5b81
Yegor <segor6052@gmail.com>
2024-08-06 22:26:17 +0300

    Add Overload file

---
+++ Test/Overload.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Products{
+private:
+	string name;
+	int price;
+	double weight;
+
+	int size;
+	int *data;
+public:
+	// Constructor;
+	Products(){
+		name = "NoName";
+		price = 0;
+		weight = 0;
+
+		size = 0;
+		data = nullptr;
+
+	}
+	// Distructor; 
+	~Products(){
+		delete [] data;
+	}
+	
+	void setName(string valueName){
+		name = valueName;
+	}
+	void setPrice(int valuePrice){
+		price = valuePrice;
+	}
+	void setWeight(double valueWeight){
+		weight = valueWeight;
+	}
+	void setData(int newSize) {
+        if (newSize > 0) {
+            delete[] data;  // Звільняємо старий масив
+            data = new int[newSize];
+            size = newSize;
+            for (int i = 0; i < size; ++i) {
+                data[i] = i + 1;
+            }
+        } else {
+            size = 0;
+            data = nullptr;
+        }
+    }
+
+    
+
+	string getName(){
+		return name;
+	}
+	int getPrice(){
+		return price;
+	}
+	double getWeight(){
+		return weight;
+	}
+	int* getData() {
+        return data;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+
+	void Print(){
+		cout << this << endl;
+		cout << "Name: " << name << "\n" << "Price: " << price << " грн" << "\n" << "Weight: " << weight / 1000 << " Кг" << "\n" << endl;
+
+		cout << "Data [ ";
+		for (int i = 0; i < size; i++){
+			cout << data[i] << " " ;
+		}
+		cout << "];" << endl;
+	}
+
+	// Peregruzka =;
+	void operator = (const Products &valueProduct){
+		cout << "Operator '='" << endl;
+		if (this->data != nullptr){
+			delete [] this->data;
+		}
+	}
+};
+
+int main(){
+
+	Products banana;
+	banana.setName("Banana");
+	banana.setPrice(90);
+	banana.setWeight(1500);
+	banana.Print();
+
+	Products persik;
+	persik = banana;
+
+	persik.Print();
+	return 0;
+}
