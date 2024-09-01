339afa817fb5ae1962c835ef25f4cd065755837c
Yegor Semenov <segor6052@gmail.com>
2024-09-01 23:32:46 +0300

    Add answer to 2022 test

--- 2022_Convert1D_arrayInto2D_Array/src/answer.cpp
+++ 2022_Convert1D_arrayInto2D_Array/src/answer.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        
+        // Кількість елементів у original співпадає з m * n ?
+        if (original.size() != m * n) {
+            return {};
+        }
+
+        // Ініціалізуємо 2D масив розміром m x n
+        vector<vector<int>> result(m, vector<int>(n));
+        for (int i = 0; i < original.size(); ++i) {
+            result[i / n][i % n] = original[i];
+        }
+
+        return result;
     }
 };
