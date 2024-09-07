71e63d7e0c9a28731950fe580786a7bdba6d104d
Yegor Semenov <segor6052@gmail.com>
2024-09-07 09:06:37 +0300

    Add smsBomber app

---
+++ Test/SmsBomber/src/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "../lib/SmsBomber.h"
+
+
+int main(){
+	SmsBomber sms1;
+
+  	sms1.setTo("+380500345698");
+    sms1.setFrom("+14158815646");
+    sms1.setBody("Не забывай дарить цветы! Лучшие цветы у нас в магазине: http://flowersacademyforyou.com/");
+
+    for (int i = 0; i < 3; i++){
+
+	    std::cout << "Massage is sanding..." << std::endl;
+	    sms1.sendSMS(sms1.getTo(), sms1.getFrom(), sms1.getBody());
+	    std::cout << "\nMassage is SAND!" << std::endl;
+    }
+
+	return 0;
+};
